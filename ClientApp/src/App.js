0497a1a8246266319403510af258017238a8022c
Luca Campoglioni <lcampoglioni@altitudo.com>
2026-04-10 12:55:44 +0200

    feat: remove Partners and Programming pages and routes

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { Partners } from './components/Partners';
-import { Programming } from './components/Programming';
 import { Staff } from './components/Staff';
 import './custom.css'
 
@@ -17,8 +15,6 @@ export default class App extends Component {
             <Layout>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/staff' component={Staff} />
-                <Route path='/partners' component={Partners} />
-                <Route path='/programming' component={Programming} />
             </Layout>
         );
     }
